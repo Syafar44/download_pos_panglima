3d630926b12a1a15d8db50a6cf2750b74bb4ed69
syafar44 <target44135@gmail.com>
2026-04-26 21:42:11 +0800

    feat versi 2.0.0

--- lib/app-versions.ts
+++ lib/app-versions.ts
@@ -16,13 +16,13 @@ export interface AppVersion {
 export const APP_VERSIONS: AppVersion[] = [
   {
     id: '1',
-    version: '1.1.7',
-    releaseDate: '20 April 2026',
-    filename: 'pos-panglima-v1.1.7.apk',
+    version: '2.0.0',
+    releaseDate: '26 April 2026',
+    filename: 'pos-panglima-v2.0.0.apk',
     size: '55 MB',
     changelog: [
-      'Rilis: Versi 1.1.7',
-      'Perbaikan: bug minor, Crash/black screen versi 2',
+      'Rilis: Versi 2.0.0',
+      'Perbaikan: bug minor, Crash/black screen pada beberapa perangkat',
     ],
     isLatest: true,
   }
